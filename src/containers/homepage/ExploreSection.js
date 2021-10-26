fd447817a665cf7c90e1f207f0e82c9fc84179d3
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-10-26 15:28:04 -0500

    create Title and SectionInfo styled components

---
+++ src/containers/homepage/ExploreSection.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+
+const ExploreSectionContainer = tw`
+    w-full
+    h-screen
+    flex
+    flex-col
+    relative
+    pt-1
+    pb-1
+    xl:pt-2
+    xl:pb-2
+    items-center
+`; 
+
+const Title = tw.h1`
+    text-2xl
+    xl:text-3xl
+    2xl:text-5xl
+    2xl:mt-6
+    2xl:mb-6
+    text-gray-800
+    font-bold
+    text-center
+`
+
+const SectionInfo = tw.p`
+    text-base
+    2xl:text-lg
+    text-gray-700 
+    text-center
+    mt-4
+    2xl:mt-2
+`; 
+
+
+function ExploreSection() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ExporeSection
